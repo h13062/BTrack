f3d4eb5ef850e3ab3d188119f5b1ad862a485b1c
H13062 <bgh1506@gmail.com>
2022-10-26 22:41:32 -0500

    Fix the eat and play service in .infrastructure

--- BabyTracker.Infrastructure/Service/EatActivityServiceAsync.cs
+++ BabyTracker.Infrastructure/Service/EatActivityServiceAsync.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BabyTracker.Core.Contract.Service;
+using BabyTracker.Core.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace BabyTracker.Infrastructure.Service
 {
-    public class EatActivityServiceAsync
+    public class EatActivityServiceAsync : IEatActivyServiceAsync
     {
+        public Task<int> AddEatAsync(EatActivityModel sleep)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<EatActivityModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EatActivityModel> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateEatAsync(EatActivityModel sleep)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
